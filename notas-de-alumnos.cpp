7b0965cbe8472c3b834e81e1b365bf944524e9a7
fulano <algo@mas.com>
2024-08-11 11:59:36 -0300

    add | maxima nota

--- notas-de-alumnos.cpp
+++ notas-de-alumnos.cpp
@@ -11,6 +11,7 @@ int main(){
         maxNota;
 
     cApro = cDes = 0;
+    maxNota = 0; // menos que la minima
 
     for(int i = 1; i <= 12; i++){
         cout << "Ingrese nota: ";
@@ -20,10 +21,15 @@ int main(){
             cDes++;
         else if(7 <= nota && nota <= 10)
             cApro++;
+
+        if(maxNota < nota){
+            maxNota = nota;
+        }
     }
 
     cout << "aprobados: " << cApro << endl;
     cout << "desaprobados: " << cDes << endl;
+    cout << "nota maxima: " << maxNota << endl;
 
     return 0;
 }
